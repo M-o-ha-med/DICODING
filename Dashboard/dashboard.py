cf7e900ab816639410189fae15293e6844a3249f
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2023-11-27 12:37:46 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
+from matplotlib import pyplot as plt
 import seaborn as sns
 plt.rcParams.update({'font.size': 15})
 
